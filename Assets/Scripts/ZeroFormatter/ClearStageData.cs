ccc9227e1223030f38a876c499399ef07d7a6a5a
Kadasumi <kar135sumi@gmail.com>
2023-01-25 23:47:01 +0900

    zeroFormatter微追加

---
+++ Assets/Scripts/ZeroFormatter/ClearStageData.cs
@@ -0,0 +1,8 @@
+using ZeroFormatter;
+
+[ZeroFormattable]
+public class ClearStageData 
+{
+   [Index(0)]
+   public virtual int StageData { get; set; }
+}
---
+++ Assets/Scripts/ZeroFormatter/DataManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    void Start()
+    {
+        ClearStageData data = new();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
